2bfeadec9678c1395b8e181d6876ea90c4e7fdfe
Juan <jmrepetti@gmail.com>
2021-01-04 00:29:42 +0100

    add Task and Choice states errors

--- lib/statex/error.ex
+++ lib/statex/error.ex
@@ -2,4 +2,10 @@ defmodule Statex.Error do
   defmodule UndefinedState do
     defexception message: "undefined state"
   end
+  defmodule MissingResourceForTaskState do
+    defexception message: "Task State must include a Resource key"
+  end
+  defmodule MissingChoicesForChoiceState do
+    defexception message: "Choice State must include a Choices key"
+  end
 end
